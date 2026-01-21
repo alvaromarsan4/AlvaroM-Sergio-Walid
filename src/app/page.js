4b716c11cefc2ccaaefff2549fbabe622a51d14e
alvaromarsan4 <alvaro.marsan.4@educa.jcyl.es>
2026-01-21 12:31:43 +0100

    Datos inicio nuevos

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,24 @@ export default function Home() {
     <div className="hero min-h-[70vh] bg-base-200 rounded-box">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold text-primary">Rick & Morty DB</h1>
+          <h1 className="text-5xl font-bold text-primary">Rick Y Morty</h1>
           
           <p className="py-6">
             Bienvenido a la aplicación de consulta de la API de Rick y Morty. 
             Desarrollado para el módulo de Desarrollo Web en Entorno Cliente.
+          </p>
+          <p>
+            Dar a Personajes (Arriba izquierda) para explorar los personajes de la serie.
+          </p>
+          <p>
+            <h2>¿Qué es Rick y Morty?</h2>
+            </p>
+            <p>Rick y Morty (en inglés: Rick and Morty) es una serie de televisión estadounidense de animación para adultos creada por Justin Roiland y Dan Harmon en 2013 para Adult Swim, también se emitió en Cartoon Network. La serie sigue las desventuras de un científico, Rick Sánchez, y su fácilmente influenciable nieto, Morty, quienes pasan el tiempo entre la vida doméstica y los viajes espaciales e intergalácticos. Dan Harmon, el cocreador de la serie y Justin Roiland son los encargados de las voces principales de Morty y Rick, la serie también incluye las voces de Chris Parnell, Spencer Grammer y Sarah Chalke.</p>
+            <h2>Argumento</h2>
+            <p>
+            Rick Sánchez es el estereotipo del «científico loco». Es un genio y buen tipo, pero es irresponsable, poco arreglado, alcohólico, egoísta, depresivo, sarcástico y con poca cordura. Rick por no poder pagar la renta de su antigua casa termina mudándose a la casa de su hija Beth y en ese momento se encuentra con su nieto Morty; un joven de 14 años sin expresión, tímido y no muy listo. Al juntarse con su nieto, Rick y Morty viven una variedad de aventuras a lo largo de universos paralelos. Y es mediante tantas vivencias y reflexiones que Rick busca que su nieto Morty no acabe como su padre, Jerry, un hombre muy poco exitoso que a pesar de tener buenas intenciones resulta ser bastante inútil en muchas ocasiones y depende mucho de su esposa, Beth.
+
+A pesar de estar muy apegados, Rick y su nieto experimentan momentos en los que Summer Smith, hermana de Morty, se une en ocasiones a las pintorescas aventuras provocadas por Rick.
           </p>
         </div>
       </div>
