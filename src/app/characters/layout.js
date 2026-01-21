dd0ceeaf4a6ed7a5fe798a7990bffe436df0e68c
alvaromarsan4 <alvaro.marsan.4@educa.jcyl.es>
2026-01-21 10:55:26 +0100

    terminado2

--- src/app/characters/layout.js
+++ src/app/characters/layout.js
@@ -1,10 +1,11 @@
 export default function CharacterLayout({ children }) {
   return (
-    <section className="animate-fadeIn">
+    <section className="animate-fadeIn px-4">
       <div className="bg-primary text-primary-content p-4 rounded-t-box mb-4">
         <h2 className="text-2xl font-bold">Explorador de Personajes</h2>
       </div>
-      <div className="p-2 bg-base-100 border border-base-200 rounded-b-box">
+
+      <div className="p-4 bg-base-100 border border-base-200 rounded-b-box">
         {children}
       </div>
     </section>
